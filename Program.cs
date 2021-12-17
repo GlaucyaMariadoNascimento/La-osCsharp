de75ed71bf246c0c61f52bfe04820c6a5f2b57f7
Glaucya <stobbeglaucya@gmail.com>
2021-12-17 12:13:05 -0300

    Resolução tabuada

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+
+namespace La_osCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, resultado;
+
+            Console.WriteLine("Digite um número para obter sua tabuada: " + "\n");
+            num = Convert.ToInt16(Console.ReadLine());
+
+            for (int cont = 1; cont <= 10; cont ++) {
+                resultado = num * cont;
+                Console.WriteLine(+ cont + " x " + num + " = " + resultado);
+
+            }
+        }
+    }
+}
